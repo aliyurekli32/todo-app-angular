951d23d834f997bd58eabb11bea3aee1249df634
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-26 04:34:43 +0300

    update: typescript issues

--- src/app/model.ts
+++ src/app/model.ts
@@ -5,17 +5,21 @@ export class Model {
     constructor(){
         this.user="Ali",
         this.items= [
+                new TodoItem(1,"Python",false),
+                new TodoItem(2,"Javascript",false),
+                new TodoItem(3,"Java",false),
 
         ]
     }
 }
+
 
 export class TodoItem {
     description;
     id;
     completed;
 
-    constructor(id,description,completed) {
+    constructor(id:number,description:string,completed:boolean) {
         this.id=id;
    this.description=description;
     this.completed=completed}
